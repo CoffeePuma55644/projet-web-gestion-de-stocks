a5392e0012f1ffc9f5f0dc716bc19b0b4a781a25
CoffeePuma55644 <coffeepuma55644@gmail.com>
2025-06-21 08:42:14 +0100

    Update script references and add main.js load log
    Changed app.html to reference app_script.js instead of main.js. Added a console log to main.js to indicate when it is loaded.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import { renderStockTable, renderSalesSummary } from './script/ui/renderer';
 import { initializeEventListeners } from './eventListeners';
 
+console.log("Le fichier main.js est chargé !")
+
 // S'assurer que le DOM est chargé avant d'exécuter le code
 document.addEventListener('DOMContentLoaded', () => {
     initializeEventListeners(); // Configurer tous les écouteurs d'événements
